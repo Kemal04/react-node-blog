4ea82b7ac7c36ff8eafd17c689c275942a803521
Kemal04 <kemalhojayew04@gmail.com>
2022-12-14 16:58:08 +0500

    home-page-banner

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import $ from 'jquery';
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className="navbar navbar-expand-lg navbar-light">
+            <div className="container">
+                <NavLink to="/" className="navbar-brand text-white fw-bold">
+                    Logo
+                </NavLink>
+                <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+                    <div className="navbar-nav me-auto ms-4">
+                        <NavLink to="/" className="nav-item nav-link text-uppercase me-3">bas sahypa</NavLink>
+                        <NavLink to="/tazelikler" className="nav-item nav-link text-uppercase me-3">Täzelikler</NavLink>
+                        <NavLink to="/makalalar" className="nav-item nav-link text-uppercase me-3">makalalar</NavLink>
+                        <NavLink to="/biz-barada" className="nav-item nav-link text-uppercase me-3">biz barada</NavLink>
+                        <NavLink to="/habarlasmak" className="nav-item nav-link text-uppercase me-3">habarlaşmak</NavLink>
+                    </div>
+                    <div className='ms-auto'>
+                        <div class="">
+                            <input type="text" placeholder='Gözleg . . .' class="form-control border-0 text-white search rounded-1 py-2 px-4" style={{ backgroundColor: "rgba(255,255,255,0.2)", outline: "none" }} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
 }
+
+$(document).ready(function () {
+    "use strict";
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('nav-sticky');
+        } else {
+            $('.navbar').removeClass('nav-sticky');
+        }
+    });
+
+});
 
 export default Navbar
--- client/src/pages/userInterface/home/Home.js
+++ client/src/pages/userInterface/home/Home.js
@@ -1,8 +1,61 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <div className='home-bg'>
+                <div className='container'>
+                    <div className='row flex-column aling-items-center justify-content-start text-white'>
+                        <div className='col-lg-1 text-center'>
+                            <div className='rounded-5 bg-secondary text-white py-1 px-3 fw-normal mt-3'>Sport</div>
+                        </div>
+                        <div className='col-lg-5 h1 fw-normal mt-3'>
+                            How Did van Gogh’s Turbulent Mind Depict One of the Most Complex Concepts in Physics?
+                        </div>
+                        <div className='col-lg-12 mt-4 disabled' style={{ color: "#afafaf" }}>
+                            Continue Reading &rarr;
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container mt-5'>
+                <div className='row'>
+                    <div className='col-lg-9'>
+                        <div className='row align-items-center'>
+                            <div className='col-lg-3'>
+                                Categories
+                            </div>
+                            <div className='col-lg-9'>
+                                <ul class="nav justify-content-end">
+                                    <li class="nav-item small mx-1">
+                                        <a class="nav-link rounded-5 py-1 px-3 text-dark bg-light">Hemmesi</a>
+                                    </li>
+                                    <li class="nav-item small mx-1">
+                                        <a class="nav-link rounded-5 py-1 px-3 text-dark" style={{ backgroundColor: "#ededed" }}>Sport</a>
+                                    </li>
+                                    <li class="nav-item small mx-1">
+                                        <a class="nav-link rounded-5 py-1 px-3 text-dark" style={{ backgroundColor: "#ededed" }}>Saglyk</a>
+                                    </li>
+                                    <li class="nav-item small mx-1">
+                                        <a class="nav-link rounded-5 py-1 px-3 text-dark" style={{ backgroundColor: "#ededed" }}>Biznes</a>
+                                    </li>
+                                    <li class="nav-item small mx-1">
+                                        <a class="nav-link rounded-5 py-1 px-3 text-dark" style={{ backgroundColor: "#ededed" }}>DÜnýäde</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='row mt-4'>
+                            
+                        </div>
+                    </div>
+                    <div className='col-lg-3'>
+
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
